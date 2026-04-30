741b91d3ba7e8acb1d1d3748a0c2b90dce17f296
Abdullah Mughal <abdullahmughalxdev@gmail.com>
2026-04-30 21:53:51 +0500

    added hero.jsx

---
+++ Multi_Page_Glocery_Website/src/components/Hero/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Grocery from "../../assets/grocery.png";
+import Button from "../Button/button";
+
+const Hero = () => {
+  return (
+    <section>
+      <div className="max-w-[1400px] mx-auto px-10 flex items-center  min-h-screen ">
+        <div className="flex-1 space-y-6 mt">
+          <span className="bg-orange-100 text-orange-500 text-lg px-5 py-2 rounded-full ">
+            Export Best Quality...
+          </span>
+          <h1 className="text-6xl/18 font-bold mt-4 ">
+            Tasty and Orgenic <span className="text-orange-500">Fruits</span> &{" "}
+            <span className="text-orange-500">Veggies</span>
+            <br /> In Your City{" "}
+          </h1>
+          <p className="text-zinc-600 text-lg max-w-[530px] mb-10">
+            Bread for a high content of beneficial substance. Our products are
+            all fresh and healthy
+          </p>
+          <Button content="Shop Now" />
+        </div>
+
+        {/* img section */}
+        <div className="flex-1">
+          <img src={Grocery} alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- Multi_Page_Glocery_Website/src/components/Navbar/Navbar.jsx
+++ Multi_Page_Glocery_Website/src/components/Navbar/Navbar.jsx
@@ -5,24 +5,24 @@ import { IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <header>
+    <header className="bg-white fixed top-0 left-0 right-0">
       <nav className="flex justify-between h-[14vh] items-center px-10 max-w-[1400px] mx-auto">
         <a href="#" className="text-3xl font-bold">
           Gr<span className="text-orange-500 uppercase">o</span>cify
         </a>
 
-        <ul className="flex items-center gap-x-15">
+        <ul className="flex items-center gap-x-15 hidden lg:flex ">
           <li>
-            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">Home</a>
+            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500 inline-block hover:scale-125 transition-all duration">Home</a>
           </li>
           <li>
-            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">About</a>
+            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500 inline-block hover:scale-125 transition-all duration">About</a>
           </li>
           <li>
-            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">Process</a>
+            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500 inline-block hover:scale-125 transition-all duration">Process</a>
           </li>  
           <li>
-            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500">Contact Us</a>
+            <a href="#" className="font-semibold tracking-wider text-zinc-800 hover:text-orange-500 inline-block hover:scale-125 transition-all duration">Contact Us</a>
           </li>
         </ul>
 

2a76c3e7f3615bdf6a10478cd4292acf1c8a2435
Abdullah Mughal <abdullahmughalxdev@gmail.com>
2026-05-08 18:10:43 +0500

    added some packages and made routes in frontend

---
+++ Full-Stack-Chat-App/Frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const SignUpPage = () => {
+  const [showPassword, setshowPassword] = useState(false)
+  const [formData, setformData] = useState({
+    fullName:"",
+  })
+  return (
+    <div>
+     
+    </div>
+  )
+}
+
+export default SignUpPage
